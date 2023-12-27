9dd306354df33898a74b792a903cdb0c143177d0
krismos64 <134715062+krismos64@users.noreply.github.com>
2023-12-27 18:12:33 +0100

    js

--- app.js
+++ app.js
@@ -4,91 +4,91 @@ const main = document.querySelector("main");
 const btnRejouer = document.querySelector(".btn-rejouer");
 
 let verrouillage = true,
-    joueurEnCours = "X";
+  joueurEnCours = "X";
 
 info.innerHTML = `Au tour de ${joueurEnCours}`;
 
 const alignementsGagnants = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
 let partieEnCours = ["", "", "", "", "", "", "", "", ""];
 
-cellules.forEach(cell => {
-    cell.addEventListener("click", clicSurCase);
+cellules.forEach((cell) => {
+  cell.addEventListener("click", clicSurCase);
 });
 
 function clicSurCase(e) {
-    const caseClique = e.target;
-    const caseIndex = caseClique.getAttribute("data-index");
+  const caseClique = e.target;
+  const caseIndex = caseClique.getAttribute("data-index");
 
-    if (partieEnCours[caseIndex] !== "" || !verrouillage) {
-        return;
-    }
+  if (partieEnCours[caseIndex] !== "" || !verrouillage) {
+    return;
+  }
 
-    partieEnCours[caseIndex] = joueurEnCours;
-    caseClique.innerHTML = joueurEnCours;
-    console.log(partieEnCours);
+  partieEnCours[caseIndex] = joueurEnCours;
+  caseClique.innerHTML = joueurEnCours;
+  console.log(partieEnCours);
 
-    validationResultats();
+  validationResultats();
 }
 
 function validationResultats() {
-    let finDePartie = false;
+  let finDePartie = false;
 
-    for (let i = 0; i < alignementsGagnants.length; i++) {
-        const alignementGagnant = alignementsGagnants[i];
-        // [0, 1, 2]
-        let a = partieEnCours[alignementGagnant[0]]; // 0
-        let b = partieEnCours[alignementGagnant[1]]; // 1
-        let c = partieEnCours[alignementGagnant[2]]; // 2
+  for (let i = 0; i < alignementsGagnants.length; i++) {
+    const alignementGagnant = alignementsGagnants[i];
+    // [0, 1, 2]
+    let a = partieEnCours[alignementGagnant[0]]; // 0
+    let b = partieEnCours[alignementGagnant[1]]; // 1
+    let c = partieEnCours[alignementGagnant[2]]; // 2
 
-        if (a === "" || b === "" || c === "") {
-            // console.log("continuer");
-            continue; // sort de la condition et continuer dans la boucle
-        }
-        if (a === b && b === c) {
-            finDePartie = true;
-            // console.log("sortie de la boucle");
-            break; // sort complètement de la boucle
-        }
+    if (a === "" || b === "" || c === "") {
+      // console.log("continuer");
+      continue; // sort de la condition et continuer dans la boucle
     }
-
-    if (finDePartie) {
-        main.style.minHeight = "610px";
-        info.innerText = `Le joueur ${joueurEnCours} a gagné 🎉`;
-        info.style.color = "springgreen";
-        verrouillage = false;
-        btnRejouer.style.display = "block";
-        // console.log("partie finie et sortie de la fonction");
-        return; // sort complètement de la fonction
+    if (a === b && b === c) {
+      finDePartie = true;
+      // console.log("sortie de la boucle");
+      break; // sort complètement de la boucle
     }
+  }
 
-    // si il n'y pas de chaîne de caractères vides dans partie en cours
-    let matchNul = !partieEnCours.includes("");
-    if (matchNul) {
-        main.style.minHeight = "560px";
-        info.innerText = `🏳️ Match nul 🏳️`;
-        info.style.color = "silver";
-        btnRejouer.style.display = "block";
-        return; // sort complètement de la fonction
-    }
+  if (finDePartie) {
+    main.style.minHeight = "610px";
+    info.innerText = `Le joueur ${joueurEnCours} a gagné 🎉`;
+    info.style.color = "springgreen";
+    verrouillage = false;
+    btnRejouer.style.display = "block";
+    // console.log("partie finie et sortie de la fonction");
+    return; // sort complètement de la fonction
+  }
 
-    changementDeJoueur();
+  // si il n'y pas de chaîne de caractères vides dans partie en cours
+  let matchNul = !partieEnCours.includes("");
+  if (matchNul) {
+    main.style.minHeight = "560px";
+    info.innerText = `🏳️ Match nul 🏳️`;
+    info.style.color = "silver";
+    btnRejouer.style.display = "block";
+    return; // sort complètement de la fonction
+  }
+
+  changementDeJoueur();
 }
 
 function changementDeJoueur() {
-    joueurEnCours = joueurEnCours === "X" ? "O" : "X";
-    info.innerText = `Au tour de ${joueurEnCours}`;
+  joueurEnCours = joueurEnCours === "X" ? "O" : "X";
+  info.innerText = `Au tour de ${joueurEnCours}`;
 }
 
 btnRejouer.addEventListener("click", () => {
-    location.reload();
-})
+  location.reload();
+});
